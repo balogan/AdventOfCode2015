89e50c871d96afc7d134939cc821fd0280ea0aca
Blake Logan <loganba@gmail.com>
2022-11-03 21:33:25 -0500

    day01 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,31 +1,24 @@
 use std::fs;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::str::Chars;
 
 /// Get the title of a Markdown file.
 ///
 /// Reads the first line of a Markdown file, strips any hashes and
 /// leading/trailing whitespace, and returns the title.
 fn read_line<R>(mut rdr: R) -> String
-    where R: BufRead,
+where
+    R: BufRead,
 {
     let mut first_line = String::new();
 
     rdr.read_line(&mut first_line).expect("Unable to read line");
 
-    // Where do the leading hashes stop?
-    let last_hash = first_line
-        .char_indices()
-        .skip_while(|&(_, c)| c == '#')
-        .next()
-        .map_or(0, |(idx, _)| idx);
-
-    // Trim the leading hashes and any whitespace
-    first_line[last_hash..].trim().into()
+    first_line
 }
 
-fn calculate_floor(line: String) -> i32 {
-    let chars = line.chars();
+fn calculate_floor(chars: Chars) -> i32 {
     let mut floor = 0;
     for c in chars {
         if c == '(' {
@@ -34,7 +27,25 @@ fn calculate_floor(line: String) -> i32 {
             floor -= 1;
         }
     }
-    return floor;
+    floor
+}
+
+fn get_basement_count(chars: Chars) -> i32 {
+    let mut floor = 0;
+    let mut basement_count = 0;
+    for c in chars {
+        if c == '(' {
+            floor += 1;
+            basement_count += 1;
+        } else {
+            floor -= 1;
+            basement_count += 1;
+        }
+        if floor == -1 {
+            return basement_count;
+        }
+    }
+    basement_count
 }
 
 /// Read the first line of the file into `title`.
@@ -48,31 +59,14 @@ fn main() {
     let buffer = BufReader::new(file);
 
     let line = read_line(buffer);
+    // TODO: deduplicate this
+    let chars = line.chars();
+    let chars2 = line.chars();
 
-    let floor = calculate_floor(line);
+    // TODO possibly collapse to single function
+    let floor = calculate_floor(chars);
+    let basement_count = get_basement_count(chars2);
 
-    println!("The floor is '{}'", floor);
-}
-
-#[cfg(test)]
-mod test {
-    use super::title_string;
-
-    #[test]
-    fn top_level_heading() {
-        assert_eq!(title_string(b"# This is a top-level heading".as_ref()),
-                   "This is a top-level heading")
-    }
-
-    #[test]
-    fn second_level_heading() {
-        assert_eq!(title_string(b"## This is a second-level heading".as_ref()),
-                   "This is a second-level heading");
-    }
-
-    #[test]
-    fn fourth_level_heading() {
-        assert_eq!(title_string(b"#### Let's jump straight to the fourth-level heading".as_ref()),
-                   "Let's jump straight to the fourth-level heading");
-    }
+    println!("The floor is {}", floor);
+    println!("The basement count is {}", basement_count);
 }
